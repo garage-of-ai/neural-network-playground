af6d7aa2618e94f57753211e8f60f9e4eee86a80
Lam Duc Anh <23020326@vnu.edu.vn>
2026-07-07 23:47:47 +0700

    commit

--- frontend/src/components/datasets/DatasetPanel.tsx
+++ frontend/src/components/datasets/DatasetPanel.tsx
@@ -1,6 +1,21 @@
+import DatasetPreview from "./DatasetPreview";
+
 function DatasetPanel() {
     return (
-        <></>
+        <div className="panel dataset-panel">
+            <div className="title">Tập dữ liệu</div>
+            <div className="dataset-carousel">
+                <button className="carousel-arrow carousel-arrow--left"/>
+                <div className="dataset-focus">
+                    <DatasetPreview />
+                    <div className="dataset-name"></div>
+                </div>
+                <button className="carousel-arrow carousel-arrow--right"/>
+            </div>
+            <button className="doodle-btn primary dataset-confirm">
+                Chọn dataset này
+            </button>
+        </div>
     )
 }
 
---
+++ frontend/src/components/datasets/DatasetPreview.tsx
@@ -0,0 +1,7 @@
+function DatasetPreview() {
+    return(
+        <></>
+    )
+}
+
+export default DatasetPreview
