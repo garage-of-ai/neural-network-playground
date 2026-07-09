80d971dfe14eac649d1628bf393769d7f8a697e1
Lam Duc Anh <23020326@vnu.edu.vn>
2026-07-09 09:37:54 +0700

    add losses skeleton

---
+++ backend/core/losses.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+### Tính loss phục vụ cho việc hiển thị ###
+
+def mse_loss(y_pred, y_true):
+    pass
+
+def bce_loss(y_pred, y_true):
+    pass
+
+def ce_loss(y_pred, y_true):
+    pass
+
+### Tính đạo hàm cho layer cuối ###
+
+def bce_ouput_grad(y_pred, y_true):
+    pass
+
+def ce_output_grad(y_pred, y_true):
+    pass
+
+def mse_output_grad(y_pred, y_true):
+    pass
+
