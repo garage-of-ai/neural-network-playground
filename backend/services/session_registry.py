9a2aa9ff27dc9a22c390d7b0797a063c18edc02f
Lam Duc Anh <23020326@vnu.edu.vn>
2026-07-09 11:58:48 +0700

    add session_registry skeleton

---
+++ backend/services/session_registry.py
@@ -0,0 +1,12 @@
+from training_session import TrainingSession
+
+_sessions: dict[str, TrainingSession] = {}
+
+def create_session(connection_id: str) -> TrainingSession:
+    pass
+
+def get_session(connection_id: str) -> TrainingSession:
+    pass
+
+def remove_session(connection_id: str) -> None:
+    pass
