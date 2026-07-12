882050a40a857e769bd77e06d29e132abf69d3d4
Lam Duc Anh <23020326@vnu.edu.vn>
2026-07-12 09:39:14 +0700

    sửa lỗi dấu ba chấm không biến mất khi giảm unit

--- netlab/src/components/architectures/nodes/EllipsisNode.tsx
+++ netlab/src/components/architectures/nodes/EllipsisNode.tsx
@@ -1,13 +1,25 @@
+import { motion } from 'motion/react'
 import type { NodeProps } from '@xyflow/react'
 import type { EllipsisNodeData } from '../useNetworkFlowGraph'
 
-function EllipsisNode({ data }: NodeProps) {
-    const { hiddenCount } = data as EllipsisNodeData
+const popScale = { type: 'spring' as const, stiffness: 420, damping: 18 }
+
+function EllipsisNode({ id, data }: NodeProps) {
+    const { hiddenCount, exiting, onExited } = data as EllipsisNodeData
 
     return (
-        <div className="layer-ellipsis" title={`còn ${hiddenCount} unit nữa`}>
+        <motion.div
+            initial={{ scale: 0.25, opacity: 0 }}
+            animate={exiting ? { scale: 0.25, opacity: 0 } : { scale: 1, opacity: 1 }}
+            transition={popScale}
+            onAnimationComplete={() => {
+                if (exiting) onExited(id)
+            }}
+            className="layer-ellipsis"
+            title={`còn ${hiddenCount} unit nữa`}
+        >
             ⋮
-        </div>
+        </motion.div>
     )
 }
 
